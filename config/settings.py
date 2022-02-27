781278fd0ac94e0c8ba7dbcddcc3e0e417cb4633
Ramishhasnn03 <ramishshah93@gmail.com>
2022-02-28 01:17:49 +0500

    DB settings updated.

--- config/settings.py
+++ config/settings.py
@@ -50,6 +50,10 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',

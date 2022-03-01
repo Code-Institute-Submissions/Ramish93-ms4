48ed061d14b07a725f4ec5fe3956dce44f68b818
Ramishhasnn03 <ramishshah93@gmail.com>
2022-03-02 01:36:16 +0500

    Static file path changed.

--- config/settings.py
+++ config/settings.py
@@ -77,7 +77,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = 'media/'

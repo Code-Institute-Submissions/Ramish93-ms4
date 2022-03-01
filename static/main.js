a172342375129d4f602e6e486c23ae19bac4ec25
Ramishhasnn03 <ramishshah93@gmail.com>
2022-03-02 01:55:40 +0500

    JS error fixed.

--- static/main.js
+++ static/main.js
@@ -4,17 +4,20 @@ fetch("/gallery/cart/checkout-config-view")
   // Initialize Stripe.js
   const stripe = Stripe(data.publicKey);
 
-  document.querySelector("#submitBtn").addEventListener("click", () => {
-    // Get Checkout Session ID
-    fetch("/gallery/cart/create-checkout-session")
-    .then((result) => { return result.json(); })
-    .then((data) => {
-      console.log(data);
-      // Redirect to Stripe Checkout
-      return stripe.redirectToCheckout({sessionId: data.sessionId})
-    })
-    .then((res) => {
-      console.log(res);
+  const btn = document.querySelector("#submitBtn");
+  if (btn) {
+    btn.addEventListener("click", () => {
+      // Get Checkout Session ID
+      fetch("/gallery/cart/create-checkout-session")
+      .then((result) => { return result.json(); })
+      .then((data) => {
+        console.log(data);
+        // Redirect to Stripe Checkout
+        return stripe.redirectToCheckout({sessionId: data.sessionId})
+      })
+      .then((res) => {
+        console.log(res);
+      });
     });
-  });
+  }
 });
